05113d33c7242467eb810e15f5d9973b24a60ec1
mizudokei <mizunotoki.halstudent@gmail.com>
2025-06-29 22:58:14 +0900

    店舗詳細表示の実装

--- app/routes.py
+++ app/routes.py
@@ -11,12 +11,12 @@ def index():
 
 @bp.route('/search')
 def search():
-    # 1. フロントエンドから送られてきたパラメータを取得
+    # フロントエンドから送られてきたパラメータを取得
     lat = request.args.get('lat') # 緯度
     lng = request.args.get('lng') # 経度
     range_code = request.args.get('range') # 検索範囲コード
 
-    # 2. ホットペッパーAPIへのリクエスト情報を作成
+    # APIへのリクエスト情報を作成
     api_key = current_app.config['API_KEY']
     api_url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
 
@@ -29,16 +29,39 @@ def search():
         'format': 'json'
     }
 
-    # 3. APIにリクエストを送信し、レスポンスを取得
+    # APIにリクエストを送信し、レスポンスを取得
     response = requests.get(api_url, params=params)
     response.raise_for_status()  # エラーがあれば例外を発生させる
 
-    # 4. レスポンスのJSONをPythonの辞書型に変換
+    # レスポンスのJSONをPythonの辞書型に変換
     search_result = response.json()
 
-    # 5. APIレスポンスから店舗情報のリストを取得する
+    # APIレスポンスから店舗情報のリストを取得する
     # .get('shop', [])とすることで、'shop'キーが存在しなくてもエラーにならず、空のリストを返す
     shops = search_result['results'].get('shop', [])
 
-    # 6. 取得した店舗リストを`results.html`に渡して、ページを生成して返す
+    # 取得した店舗リストを`results.html`に渡して、ページを生成して返す
     return render_template('results.html', shops=shops)
+
+@bp.route('/shop/<string:shop_id>')
+def shop_detail(shop_id):
+
+    # APIへのリクエスト情報を作成
+    api_key = current_app.config['API_KEY']
+    api_url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
+
+    params = {
+        'key': api_key,
+        'id': shop_id,
+        'format': 'json'
+    }
+
+    # APIにリクエストを送信し、レスポンスを取得
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
+    result = response.json()
+
+    # 店舗情報が取得できればその情報を、できなければNoneをshop変数に格納
+    shop = result['results']['shop'][0] if result['results']['shop'] else None
+
+    return render_template('detail.html', shop=shop)
